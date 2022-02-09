13edb6be07623f3a853e08b61ceaf8096a57bd63
Anoosragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2022-02-09 16:00:44 +0530

    RMX1941: Pick some test features (#4)

--- RMX1941.sh
+++ RMX1941.sh
@@ -3,12 +3,25 @@ cd packages/modules/Wifi
 git fetch https://github.com/AOSP-12-RMX2020/packages_modules_Wifi
 git cherry-pick c6e404695bc451a9667f4893501ef8fe78e1a0b7^..90fc3f6781171dc27fed16b60575f9ea62f02e7a 
 cd -
+
 cd frameworks/opt/telephony 
 git fetch https://github.com/phhusson/platform_frameworks_opt_telephony 
 git cherry-pick 6f116d4cdb716072261ecfe532da527182f6dad6 
 cd -
 
+# Experiments
+cd packages/apps/Settings
+git fetch https://github.com/cArN4gEisDeD/packages_apps_Settings
+git cherry-pick e730400a3fdfe1c79f7c13726a913f07615daca5
+cd -
+
+cd frameworks/base
+git fetch https://github.com/cArN4gEisDeD/frameworks_base
+git cherry-pick 956bcec8a12d0a1781f1286ede7d94769dee578d^..ad7d21fb29473209d5765c209a67348fa384d949
+cd -
+
 # Brightness Patch 
 cd frameworks/base 
 wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/0001-brightness-curve.patch 
-patch -p1 < *.patch && cd -
+patch -p1 < *.patch
+cd -
--- RMX1941_end.sh
+++ RMX1941_end.sh
@@ -1,6 +1,7 @@
 rm -rf packages/modules/Wifi
 rm -rf frameworks/base
 rm -rf frameworks/opt/telephony
+rm -rf packages/apps/Settings
 
 rm -rf .repo/local_manifests
 repo sync -c --force-sync --no-tags --no-clone-bundle --prune --force-remove-dirty
