df45a69a637e4ce70d552e2bb2e1335cdac05541
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-02-09 17:40:18 +0600

    beryllium: Pick some test features
    - display cutout utils
    - remove unnecessary cutout rros

--- beryllium.sh
+++ beryllium.sh
@@ -4,3 +4,7 @@ cd ../..
 cd system/sepolicy
 curl https://github.com/sajidshahriar72543/system_sepolicy/commit/8eb5a4b7532fdef4ad5923250e1896f13431e2c8.patch | git am -3
 cd ../..
+cd frameworks/base
+curl https://github.com/sajidshahriar72543/frameworks_base/commit/934aea887c4846184e29570e2e53b7658d8183f3.patch | git am -3
+curl https://github.com/sajidshahriar72543/frameworks_base/commit/a120e1dbf7aa3906206185ab216ddadf0808c038.patch | git am -3
+cd ../..
--- beryllium_end.sh
+++ beryllium_end.sh
@@ -1,4 +1,5 @@
 rm -rf build/make
 rm -rf system/sepolicy
+rm -rf frameworks/base
 rm -rf .repo/local_manifests
 repo sync -c --force-sync --no-tags --no-clone-bundle --prune --force-remove-dirty
